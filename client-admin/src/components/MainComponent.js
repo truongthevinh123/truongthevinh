8f7dd37f675cec3a95b560982164aa3162d404df
truongthevinh123 <truongthevinh94ltv@gmail.com>
2023-07-14 23:29:13 +0700

    1

---
+++ client-admin/src/components/MainComponent.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import MyContext from '../contexts/MyContext';
+import Menu from './MenuComponent';
+import Home from './HomeComponent';
+import Category from './CategoryComponent';
+import Product from './ProductComponent';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Order from './OrderComponent';
+import Customer from './CustomerComponent';
+class Main extends Component {
+  static contextType = MyContext; // using this.context to access global state
+  render() {
+    if (this.context.token !== '') {
+      return (
+        <div className="body-admin">
+          <Menu />
+            <Routes>
+                <Route path='/admin' element={<Navigate replace to='/admin/home' />} />
+                <Route path='/admin/home' element={<Home />} />
+                <Route path='/admin/category' element={<Category />} />
+                <Route path='/admin/product' element={<Product />} />
+                <Route path='/admin/order' element={<Order />} />
+                <Route path='/admin/customer' element={<Customer />} />
+            </Routes>
+        </div>
+      );
+    }
+    return (<div />);
+  }
+}
+export default Main;
---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.hbyozn2.mongodb.net',
+  DB_USER: 'truongthevinh94ltv',
+  DB_PASS: 'vinh123',
+  DB_DATABASE: 'shoppingonline',
+  EMAIL_USER: 'truongthevinh94ltv@gmail.com', // Microsoft mail service
+  EMAIL_PASS: 'azmidorafk09',
+  JWT_SECRET: '<jwt_secret>',   
+  JWT_EXPIRES: '60000000', // in milliseconds
+};
+module.exports = MyConstants;
